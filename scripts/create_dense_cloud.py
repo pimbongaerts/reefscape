dacca193676ad49cbc1a511de1d4dbbe7622553a
Pim Bongaerts <pimbongaerts@gmail.com>
2020-08-12 01:19:41 -0700

    Corrected script

--- scripts/create_dense_cloud.py
+++ scripts/create_dense_cloud.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 """
-~/tools/metashape-pro/metashape.sh -r ~/protocols/scripts/create_dense_cloud.py project_name camera_path
+./metashape.sh -r create_dense_cloud.py [project_name] [camera_path]
+~/tools/metashape-pro/metashape.sh -r ~/protocols/scripts/create_dense_cloud.py ~/mounts/coral3d/Seaquarium/test/test_project2.psx ~/mounts/coral3d/Seaquarium/test/test.raw
+
 """
 import Metashape
 import argparse
@@ -15,7 +17,7 @@ __license__ = 'GPL'
 CAMERA_EXTENSION = 'jpeg'
 
 def get_cameras(camera_path):
-    """ get the photo (.JPG) list in specified folder """
+    """ Get the paths for each camera """
     camera_list = []
     for filename in os.listdir(camera_path):
         if filename.endswith('.' + CAMERA_EXTENSION):
@@ -23,25 +25,15 @@ def get_cameras(camera_path):
             camera_list.append(filepath)
     return camera_list
 
-def save_project(project_name):
-    """ Save Metashape project """
-    try:
-        doc.save(project_name)
-        print("Created new Metashape project: {0}".format(project_name))
-    except RuntimeError:
-        print("Could not save new Metashape project: {0}".format(project_name))
-        exit()
-
 def main(project_name, camera_path):
 
     doc = Metashape.app.document
+    doc.save(project_name)
+
     chunk = doc.addChunk()
     chunk.crs = Metashape.CoordinateSystem("EPSG::4612")
-    save_project(project_name)
-
     chunk.addPhotos(get_cameras(camera_path))
-
-    exit()
+    doc.save()
 
     chunk.matchPhotos(downscale = 1,                    # Image alignment accuracy = High
                       generic_preselection = True,      # Enable generic preselection
@@ -49,25 +41,25 @@ def main(project_name, camera_path):
                       filter_mask = False,              # Disable filtering points by mask
                       mask_tiepoints = False,           # Disable applying mask filter to tie points
                       keypoint_limit = 5000,            
-                      tiepoint_limit = 5000,
+                      tiepoint_limit = 0,
                       keep_keypoints = False,           # Do not store keypoints in the project
                       guided_matching = False,          # Disable guided image matching
                       reset_matches = True)             # Resent current matches
-    save_project(project_name)
+    doc.save()
 
     chunk.alignCameras(adaptive_fitting = True,         # Enable adaptive fitting of distortion coefficients
                        reset_alignment = True)          # Reset current alignment
-    save_project(project_name)
+    doc.save()
 
-    chunk.buildDepthMaps(downscale = 1,                 # Depth map quality = Ultra (1)
+    chunk.buildDepthMaps(downscale = 2,                 # Depth map quality = High (2)
                          filter_mode = Metashape.MildFiltering,
                          reuse_depth = False)           # Disable reuse depth maps option
-    save_project(project_name)
+    doc.save()
 
     chunk.buildDenseCloud(point_colors = True,          # Enable point colors calculation
                           point_confidence = True,      # Enable point confidence calculation
                           keep_depth = True)            # Enable store depth maps option
-    save_project(project_name)
+    doc.save()
 
 
 if __name__ == '__main__':
