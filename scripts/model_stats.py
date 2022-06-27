90c495ced1917b00ee168f53743f8e3003dbcc08
Pim Bongaerts <pim@calacademy.org>
2022-06-27 14:16:56 +1200

    Added marker count

--- scripts/model_stats.py
+++ scripts/model_stats.py
@@ -76,6 +76,7 @@ class Timepoint(object):
                 cams_not_aligned = 0
                 cams_total = 0
                 pointcount = 0
+                markercount = 0
             model_stats = open('model_stats.csv', 'a')
             model_stats.write('{0},{1},{2},{3},{4}\n'.format(short_name, cams_not_aligned, 
                                                          cams_total, pointcount, markercount))
