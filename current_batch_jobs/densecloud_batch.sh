eb4057f25d63463ad6e841057c6933b0d3492172
Pim Bongaerts <pim@calacademy.org>
2023-02-20 11:52:42 +1300

    Changed script

--- current_batch_jobs/densecloud_batch.sh
+++ current_batch_jobs/densecloud_batch.sh
@@ -1,56 +1,56 @@
 cd /mnt/coral3d/focal_plots/cur_kal/cur_kal_20m/cur_kal_20m_20221206
-~/tools/metashape-pro-deepsquid-1.8/metashape.sh -platform offscreen -r ~/reefscape/scripts/create_dense_cloud.py
+~/tools/metashape-deepsquid-1.8/metashape.sh  -platform offscreen -r ~/reefscape/scripts/create_dense_cloud.py
 cd /mnt/coral3d/focal_plots/cur_kal/cur_kal_40m/cur_kal_40m_20221217
-~/tools/metashape-pro-deepsquid-1.8/metashape.sh -platform offscreen -r ~/reefscape/scripts/create_dense_cloud.py
+~/tools/metashape-deepsquid-1.8/metashape.sh  -platform offscreen -r ~/reefscape/scripts/create_dense_cloud.py
 cd /mnt/coral3d/focal_plots/cur_kal/cur_kal_10m/cur_kal_10m_20221206
-~/tools/metashape-pro-deepsquid-1.8/metashape.sh -platform offscreen -r ~/reefscape/scripts/create_dense_cloud.py
+~/tools/metashape-deepsquid-1.8/metashape.sh  -platform offscreen -r ~/reefscape/scripts/create_dense_cloud.py
 cd /mnt/coral3d/focal_plots/cur_kal/cur_kal_05m/cur_kal_05m_20221206
-~/tools/metashape-pro-deepsquid-1.8/metashape.sh -platform offscreen -r ~/reefscape/scripts/create_dense_cloud.py
+~/tools/metashape-deepsquid-1.8/metashape.sh  -platform offscreen -r ~/reefscape/scripts/create_dense_cloud.py
 cd /mnt/coral3d/focal_plots/cur_kal/cur_kal_60m/cur_kal_60m_20221217
-~/tools/metashape-pro-deepsquid-1.8/metashape.sh -platform offscreen -r ~/reefscape/scripts/create_dense_cloud.py
+~/tools/metashape-deepsquid-1.8/metashape.sh  -platform offscreen -r ~/reefscape/scripts/create_dense_cloud.py
 cd /mnt/coral3d/focal_plots/cur_sna/cur_sna_20m/cur_sna_20m_20221205
-~/tools/metashape-pro-deepsquid-1.8/metashape.sh -platform offscreen -r ~/reefscape/scripts/create_dense_cloud.py
+~/tools/metashape-deepsquid-1.8/metashape.sh  -platform offscreen -r ~/reefscape/scripts/create_dense_cloud.py
 cd /mnt/coral3d/focal_plots/cur_sna/cur_sna_10m/cur_sna_10m_20221205
-~/tools/metashape-pro-deepsquid-1.8/metashape.sh -platform offscreen -r ~/reefscape/scripts/create_dense_cloud.py
+~/tools/metashape-deepsquid-1.8/metashape.sh  -platform offscreen -r ~/reefscape/scripts/create_dense_cloud.py
 cd /mnt/coral3d/focal_plots/cur_sna/cur_sna_05m/cur_sna_05m_20221205
-~/tools/metashape-pro-deepsquid-1.8/metashape.sh -platform offscreen -r ~/reefscape/scripts/create_dense_cloud.py
+~/tools/metashape-deepsquid-1.8/metashape.sh  -platform offscreen -r ~/reefscape/scripts/create_dense_cloud.py
 cd /mnt/coral3d/focal_plots/cur_sna/cur_sna_40m/cur_sna_40m_20221223
-~/tools/metashape-pro-deepsquid-1.8/metashape.sh -platform offscreen -r ~/reefscape/scripts/create_dense_cloud.py
+~/tools/metashape-deepsquid-1.8/metashape.sh  -platform offscreen -r ~/reefscape/scripts/create_dense_cloud.py
 cd /mnt/coral3d/focal_plots/cur_sna/cur_sna_60m/cur_sna_60m_20221223
-~/tools/metashape-pro-deepsquid-1.8/metashape.sh -platform offscreen -r ~/reefscape/scripts/create_dense_cloud.py
+~/tools/metashape-deepsquid-1.8/metashape.sh  -platform offscreen -r ~/reefscape/scripts/create_dense_cloud.py
 cd /mnt/coral3d/focal_plots/cur_sea/cur_sea_10m/cur_sea_10m_20221125
-~/tools/metashape-pro-deepsquid-1.8/metashape.sh -platform offscreen -r ~/reefscape/scripts/create_dense_cloud.py
+~/tools/metashape-deepsquid-1.8/metashape.sh  -platform offscreen -r ~/reefscape/scripts/create_dense_cloud.py
 cd /mnt/coral3d/focal_plots/cur_sea/cur_sea_20m/cur_sea_20m_20221125
-~/tools/metashape-pro-deepsquid-1.8/metashape.sh -platform offscreen -r ~/reefscape/scripts/create_dense_cloud.py
+~/tools/metashape-deepsquid-1.8/metashape.sh  -platform offscreen -r ~/reefscape/scripts/create_dense_cloud.py
 cd /mnt/coral3d/focal_plots/cur_sea/cur_sea_40m/cur_sea_40m_20221202
-~/tools/metashape-pro-deepsquid-1.8/metashape.sh -platform offscreen -r ~/reefscape/scripts/create_dense_cloud.py
+~/tools/metashape-deepsquid-1.8/metashape.sh  -platform offscreen -r ~/reefscape/scripts/create_dense_cloud.py
 cd /mnt/coral3d/focal_plots/cur_sea/cur_sea_60m/cur_sea_60m_20221202
-~/tools/metashape-pro-deepsquid-1.8/metashape.sh -platform offscreen -r ~/reefscape/scripts/create_dense_cloud.py
+~/tools/metashape-deepsquid-1.8/metashape.sh  -platform offscreen -r ~/reefscape/scripts/create_dense_cloud.py
 cd /mnt/coral3d/focal_plots/cur_dir/cur_dir_05m/cur_dir_05m_20221129
-~/tools/metashape-pro-deepsquid-1.8/metashape.sh -platform offscreen -r ~/reefscape/scripts/create_dense_cloud.py
+~/tools/metashape-deepsquid-1.8/metashape.sh  -platform offscreen -r ~/reefscape/scripts/create_dense_cloud.py
 cd /mnt/coral3d/focal_plots/cur_dir/cur_dir_10m/cur_dir_10m_20221129
-~/tools/metashape-pro-deepsquid-1.8/metashape.sh -platform offscreen -r ~/reefscape/scripts/create_dense_cloud.py
+~/tools/metashape-deepsquid-1.8/metashape.sh  -platform offscreen -r ~/reefscape/scripts/create_dense_cloud.py
 cd /mnt/coral3d/focal_plots/cur_dir/cur_dir_20m/cur_dir_20m_20221129
-~/tools/metashape-pro-deepsquid-1.8/metashape.sh -platform offscreen -r ~/reefscape/scripts/create_dense_cloud.py
+~/tools/metashape-deepsquid-1.8/metashape.sh  -platform offscreen -r ~/reefscape/scripts/create_dense_cloud.py
 cd /mnt/coral3d/focal_plots/cur_dir/cur_dir_40m/cur_dir_40m_20221203
-~/tools/metashape-pro-deepsquid-1.8/metashape.sh -platform offscreen -r ~/reefscape/scripts/create_dense_cloud.py
+~/tools/metashape-deepsquid-1.8/metashape.sh  -platform offscreen -r ~/reefscape/scripts/create_dense_cloud.py
 cd /mnt/coral3d/focal_plots/cur_dir/cur_dir_60m/cur_dir_60m_20221203
-~/tools/metashape-pro-deepsquid-1.8/metashape.sh -platform offscreen -r ~/reefscape/scripts/create_dense_cloud.py
+~/tools/metashape-deepsquid-1.8/metashape.sh  -platform offscreen -r ~/reefscape/scripts/create_dense_cloud.py
 cd /mnt/coral3d/focal_plots/cur_wat/cur_wat_05m/cur_wat_05m_20221128
-~/tools/metashape-pro-deepsquid-1.8/metashape.sh -platform offscreen -r ~/reefscape/scripts/create_dense_cloud.py
+~/tools/metashape-deepsquid-1.8/metashape.sh  -platform offscreen -r ~/reefscape/scripts/create_dense_cloud.py
 cd /mnt/coral3d/focal_plots/cur_wat/cur_wat_10m/cur_wat_10m_20221128
-~/tools/metashape-pro-deepsquid-1.8/metashape.sh -platform offscreen -r ~/reefscape/scripts/create_dense_cloud.py
+~/tools/metashape-deepsquid-1.8/metashape.sh  -platform offscreen -r ~/reefscape/scripts/create_dense_cloud.py
 cd /mnt/coral3d/focal_plots/cur_wat/cur_wat_20m/cur_wat_20m_20221128
-~/tools/metashape-pro-deepsquid-1.8/metashape.sh -platform offscreen -r ~/reefscape/scripts/create_dense_cloud.py
+~/tools/metashape-deepsquid-1.8/metashape.sh  -platform offscreen -r ~/reefscape/scripts/create_dense_cloud.py
 cd /mnt/coral3d/focal_plots/cur_hul/cur_hul_05m/cur_hul_05m_20221208
-~/tools/metashape-pro-deepsquid-1.8/metashape.sh -platform offscreen -r ~/reefscape/scripts/create_dense_cloud.py
+~/tools/metashape-deepsquid-1.8/metashape.sh  -platform offscreen -r ~/reefscape/scripts/create_dense_cloud.py
 cd /mnt/coral3d/focal_plots/cur_hul/cur_hul_10m/cur_hul_10m_20221208
-~/tools/metashape-pro-deepsquid-1.8/metashape.sh -platform offscreen -r ~/reefscape/scripts/create_dense_cloud.py
+~/tools/metashape-deepsquid-1.8/metashape.sh  -platform offscreen -r ~/reefscape/scripts/create_dense_cloud.py
 cd /mnt/coral3d/focal_plots/cur_hul/cur_hul_20m/cur_hul_20m_20221208
-~/tools/metashape-pro-deepsquid-1.8/metashape.sh -platform offscreen -r ~/reefscape/scripts/create_dense_cloud.py
+~/tools/metashape-deepsquid-1.8/metashape.sh  -platform offscreen -r ~/reefscape/scripts/create_dense_cloud.py
 cd /mnt/coral3d/focal_plots/cur_est/cur_est_05m/cur_est_05m_20221201
-~/tools/metashape-pro-deepsquid-1.8/metashape.sh -platform offscreen -r ~/reefscape/scripts/create_dense_cloud.py
+~/tools/metashape-deepsquid-1.8/metashape.sh  -platform offscreen -r ~/reefscape/scripts/create_dense_cloud.py
 cd /mnt/coral3d/focal_plots/cur_est/cur_est_10m/cur_est_10m_20221201
-~/tools/metashape-pro-deepsquid-1.8/metashape.sh -platform offscreen -r ~/reefscape/scripts/create_dense_cloud.py
+~/tools/metashape-deepsquid-1.8/metashape.sh  -platform offscreen -r ~/reefscape/scripts/create_dense_cloud.py
 cd /mnt/coral3d/focal_plots/cur_est/cur_est_20m/cur_est_20m_20221201
-~/tools/metashape-pro-deepsquid-1.8/metashape.sh -platform offscreen -r ~/reefscape/scripts/create_dense_cloud.py
+~/tools/metashape-deepsquid-1.8/metashape.sh  -platform offscreen -r ~/reefscape/scripts/create_dense_cloud.py
