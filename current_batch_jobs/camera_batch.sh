b4b5cef4b662e494f7f15ef9ca950ce6fb3fe92c
Pim Bongaerts <pimbongaerts@gmail.com>
2023-05-12 00:33:39 -0700

    Create batch job scripts

---
+++ current_batch_jobs/camera_batch.sh
@@ -0,0 +1,8 @@
+cd /mnt/coral3d/focal_plots/cur_sna/cur_sna_20m/cur_sna_20m_20230414
+python3 ~/reefscape/scripts/convert_cameras.py
+cd /mnt/coral3d/focal_plots/cur_sna/cur_sna_10m/cur_sna_10m_20230414
+python3 ~/reefscape/scripts/convert_cameras.py
+cd /mnt/coral3d/focal_plots/cur_sna/cur_sna_40m/cur_sna_40m_20230414
+python3 ~/reefscape/scripts/convert_cameras.py
+cd /mnt/coral3d/focal_plots/cur_sna/cur_sna_60m/cur_sna_60m_20230414
+python3 ~/reefscape/scripts/convert_cameras.py
